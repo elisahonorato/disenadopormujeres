f0f266ba07959235bccbe8abf5da16f465f245ab
Elisa <elisahonoratolira@uc.cl>
2023-08-15 17:34:32 -0400

    about

--- src/component/about.js
+++ src/component/about.js
@@ -1,8 +1,28 @@
-// About.js
 import React from 'react';
-  
-function About (){
-    return <h1>Welcome to the world of About!</h1>
+import { Container, Typography, Grid } from '@mui/material';
+import aboutImage from '../assets/images/equipo/miembro1.jpg';
+function About() {
+  return (
+    <Container maxWidth="xl" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
+      <Grid container spacing={4}>
+        {/* Photo */}
+        {/* Text */}
+        <Grid item xs={12} md={6}>
+          <Typography variant="h1" gutterBottom>
+            Welcome to the World of About!
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod eget nisl eu facilisis.
+            Fusce et nunc vel leo vestibulum dictum. Duis vel justo maximus, scelerisque risus sit amet,
+            vestibulum lorem. Sed tristique dui non nisl faucibus, et auctor eros vulputate.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <img src={aboutImage} alt="About Us" style={{ width: '100%', height: 'auto', }} />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
-  
+
 export default About;
