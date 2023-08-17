bff870a461586c7f72ddd153246a20f4a34135c2
Elisa <elisahonoratolira@uc.cl>
2023-08-17 18:24:31 -0400

    case study

--- src/component/casestudy.js
+++ src/component/casestudy.js
@@ -26,7 +26,7 @@ function CaseStudy() {
   const itemsPerLoad = 1; // Number of items to load each time
 
   const handleScroll = () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    if (window.innerHeight*(6/5) + window.scrollY >= document.body.offsetHeight) {
       // Load more cases when user reaches the bottom
       setVisibleCases((prevVisibleCases) =>
         Math.min(prevVisibleCases + itemsPerLoad, caseStudies.length)
@@ -45,29 +45,29 @@ function CaseStudy() {
     <Container maxWidth="xl" style={{ paddingTop: theme.spacing(4) }}>
       <Box display="flex" flexWrap="wrap" justifyContent="center">
         {caseStudies.slice(0, visibleCases).map((study, index) => (
-          <Grid item key={index} xs={12} sm={6} md={3} lg={2} style={{ display: 'flex', justifyContent: 'center' }}>
+          <Grid item key={index} xs={12} sm={6} md={3} lg={2} style={{ display: 'flex', justifyContent: 'center', marginBottom: '10vh'}}>
             {/* Use the Fade component to achieve fade-in effect */}
             <Fade in={true} timeout={500}>
               <Box display="flex" flexDirection="column" alignItems="left" p={2}>
-                <img src={study.image} alt={study.titulo} style={{ maxHeight: '400px', width: 'auto', objectFit: 'contain' }} />
+                <img src={study.image} alt={study.titulo} style={{ maxHeight: '400px', width: 'auto', objectFit: 'contain', marginBottom: '3vh' }} />
                 <Typography variant="body1" align="left">
-                  <span>Título: </span>
+                  <strong>Título: </strong>
                   {study.titulo}
                 </Typography>
                 <Typography variant="body1" align="left">
-                  <span>Formato: </span>
+                  <strong>Formato: </strong>
                   {study.formato}
                 </Typography>
                 <Typography variant="body1" align="left">
-                  <span>Autor: </span>
+                  <strong>Autor: </strong>
                   {study.autor}
                 </Typography>
                 <Typography variant="body1" align="left">
-                  <span>Año: </span>
+                  <strong>Año: </strong>
                   {study.año}
                 </Typography>
                 <Typography variant="body1" align="left">
-                  <span>Archivo: </span>
+                  <strong>Archivo: </strong>
                   {study.archivo}
                 </Typography>
 
