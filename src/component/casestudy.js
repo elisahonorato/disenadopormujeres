3e1d36784d573e411184699408ff078d6b43a8b1
Elisa <elisahonoratolira@uc.cl>
2023-08-17 16:30:24 -0400

    case study

--- src/component/casestudy.js
+++ src/component/casestudy.js
@@ -1,37 +1,81 @@
-import React from 'react';
-import { Container, Typography, Grid } from '@mui/material';
-import miembro1 from '../assets/images/equipo/miembro1.jpg';
-import miembro2 from '../assets/images/equipo/miembro2.jpg';
+import React, { useState, useEffect } from 'react';
+import { Container, Typography, Grid, Fade, Box } from '@mui/material';
+import case1 from '../assets/images/casestudy/case1.png';
+import case2 from '../assets/images/casestudy/case1.png';
+
 import theme from '../theme';
 
+const caseStudies = [
+  { image: case1, titulo: 'Miembro 1', formato: 'Afiche', autor: 'Ximena Ulibarri', año: '1976', archivo: 'Ximena Ulibarri' },
+  { image: case2, titulo: 'Miembro 1', formato: 'Afiche', autor: 'Ximena Ulibarri', año: '1976', archivo: 'Ximena Ulibarri' },
+  { image: case1, titulo: 'Miembro 1', formato: 'Afiche', autor: 'Ximena Ulibarri', año: '1976', archivo: 'Ximena Ulibarri' },
+  { image: case2, titulo: 'Miembro 1', formato: 'Afiche', autor: 'Ximena Ulibarri', año: '1976', archivo: 'Ximena Ulibarri' },
+  { image: case1, titulo: 'Miembro 1', formato: 'Afiche', autor: 'Ximena Ulibarri', año: '1976', archivo: 'Ximena Ulibarri' },
+  { image: case2, titulo: 'Miembro 1', formato: 'Afiche', autor: 'Ximena Ulibarri', año: '1976', archivo: 'Ximena Ulibarri' },
+  { image: case2, titulo: 'Miembro 1', formato: 'Afiche', autor: 'Ximena Ulibarri', año: '1976', archivo: 'Ximena Ulibarri' },
+  { image: case1, titulo: 'Miembro 1', formato: 'Afiche', autor: 'Ximena Ulibarri', año: '1976', archivo: 'Ximena Ulibarri' },
+  { image: case2, titulo: 'Miembro 1', formato: 'Afiche', autor: 'Ximena Ulibarri', año: '1976', archivo: 'Ximena Ulibarri' },
 
-const teamMembers = [
-  { image: miembro1, name: 'Miembro 1', position: 'Cargo 1' },
-  { image: miembro2, name: 'Miembro 2', position: 'Cargo 2' },
-  { image: miembro2, name: 'Miembro 2', position: 'Cargo 2' },
-  { image: miembro2, name: 'Miembro 2', position: 'Cargo 2' },
-  { image: miembro2, name: 'Miembro 2', position: 'Cargo 2' },
-  { image: miembro2, name: 'Miembro 2', position: 'Cargo 2' },
-  { image: miembro2, name: 'Miembro 2', position: 'Cargo 2' },
+
+
   // Add more team members here
 ];
 
 function CaseStudy() {
+  const [visibleCases, setVisibleCases] = useState(3); // Initial number of visible members
+  const itemsPerLoad = 1; // Number of items to load each time
+
+  const handleScroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      // Load more cases when user reaches the bottom
+      setVisibleCases((prevVisibleCases) =>
+        Math.min(prevVisibleCases + itemsPerLoad, caseStudies.length)
+      );
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <Container maxWidth="xl" style={{ paddingTop: theme.spacing(4) }}>
-      <Grid container spacing={4}>
-        {teamMembers.map((member, index) => (
-          <Grid item key={index} xs={12} sm={6} md={3} sx={{zIndex: 2000000 }}>
-            <div style={{ textAlign: 'left'}}>
-              <div style={{ width: '100%', height: '150px', overflow: 'hidden', margin: '0 auto' }}>
-                <img src={member.image} alt={member.name} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-              </div>
-              <Typography variant="h4" sx={{ paddingTop: theme.spacing(3), lineHeight: theme.spacing(2) }}>{member.name}</Typography>
-              <Typography variant="h4" color="textSecondary">{member.position}</Typography>
-            </div>
+      <Box display="flex" flexWrap="wrap" justifyContent="center">
+        {caseStudies.slice(0, visibleCases).map((study, index) => (
+          <Grid item key={index} xs={12} sm={6} md={3} lg={2} style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* Use the Fade component to achieve fade-in effect */}
+            <Fade in={true} timeout={500}>
+              <Box display="flex" flexDirection="column" alignItems="left" p={2}>
+                <img src={study.image} alt={study.titulo} style={{ maxHeight: '400px', width: 'auto', objectFit: 'contain' }} />
+                <Typography variant="body1" align="left">
+                  <span>Título: </span>
+                  {study.titulo}
+                </Typography>
+                <Typography variant="body1" align="left">
+                  <span>Formato: </span>
+                  {study.formato}
+                </Typography>
+                <Typography variant="body1" align="left">
+                  <span>Autor: </span>
+                  {study.autor}
+                </Typography>
+                <Typography variant="body1" align="left">
+                  <span>Año: </span>
+                  {study.año}
+                </Typography>
+                <Typography variant="body1" align="left">
+                  <span>Archivo: </span>
+                  {study.archivo}
+                </Typography>
+
+              </Box>
+            </Fade>
           </Grid>
         ))}
-      </Grid>
+      </Box>
     </Container>
   );
 }
