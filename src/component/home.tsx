74e55e06398d1e88b41677f4d74bd9576ed16103
Elisa <elisahonoratolira@uc.cl>
2023-10-20 17:22:26 -0300

    style: Update about homepage

--- src/component/home.tsx
+++ src/component/home.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Grid, Paper} from '@mui/material';
+import { Container, Typography, Grid, Paper } from '@mui/material';
 import aboutImage from '../assets/images/home/xime.jpg';
+import revistaImage from '../assets/images/home/revista.png';
 import theme from '../theme.tsx';
 import ColorFilter from './colorfilter.tsx';
 import ColorStain from './colorstain.tsx';
@@ -9,7 +10,6 @@ import { purple } from '@mui/material/colors';
 // Definir un tipo personalizado para 'position'
 type Position = 'sticky' | 'absolute';
 
-
 function Home() {
   const [positionStyle, setPositionStyle] = useState<Position>('sticky');
   const [width, setWidth] = useState('100%');
@@ -17,7 +17,6 @@ function Home() {
     zIndex: 1,
     width: width,
   };
-  
 
   useEffect(() => {
     const handleWindowResize = () => {
@@ -41,27 +40,36 @@ function Home() {
 
   return (
     <Container maxWidth="xl">
-      <ColorStain 
-              color={purple[600]}
-              marginTop={'90px'}
-              marginLeft={'calc(50% + 20vh)'}
-              zIndex={10000}
-            />
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={6}>
-          <div
-            style={{
-              ...textOverlayStyles,
-              position: positionStyle,
-            }}
-          >
-            <Typography variant="h2" marginTop={theme.spacing(4)} marginLeft={3}>
-            Diseñado por Mujeres es un proyecto de investigación sobre mujeres pioneras del Diseño en Chile, a nivel de prácticas y discursos, para la construcción de un entorno disciplinar con equidad de género
-            </Typography>
-          </div>
+      <ColorStain
+        color={purple[600]}
+        marginTop={'90px'}
+        marginLeft={'calc(50% + 20vh)'}
+        zIndex={10000}
+      />
+      <Grid container spacing={4} direction='row'> {/* grilla general */}
+        <Grid item xs={12} md={4}> {/* grilla texto */}
+          <Paper elevation={0}>
+            <div
+              style={{
+                ...textOverlayStyles,
+                position: positionStyle,
+                padding: theme.spacing(2),
+              }}
+            >
+              <Typography variant="h2">
+                Diseñado por Mujeres es un proyecto de investigación sobre mujeres pioneras del Diseño en Chile, a nivel de prácticas y discursos, para la construcción de un entorno disciplinar con equidad de género
+              </Typography>
+            </div>
+          </Paper>
         </Grid>
-        <Grid item xs={12} md={6} textAlign={'right'}>
-          <ColorFilter image={aboutImage} alt='Ximena Ulibarri' customStyles={{ width: '79%', height: 'inherit', objectFit: 'cover'}} hover={false}/>
+        {/* Contenedor para las imágenes */}
+        <Grid item xs={12} md={8} container direction="row" alignItems={'self-end'}>
+          <Grid item xs={4}>
+            <ColorFilter image={revistaImage} alt='Ximena Ulibarri' customStyles={{ width: '140%', height: 'auto', objectFit: 'cover' }} hover={false} />
+          </Grid>
+          <Grid item xs={7}>
+            <ColorFilter image={aboutImage} alt='Revista' customStyles={{ width: '110%', height: 'inherit', objectFit: 'cover' }} hover={false} />
+          </Grid>
         </Grid>
       </Grid>
     </Container>
@@ -69,3 +77,4 @@ function Home() {
 }
 
 export default Home;
+
